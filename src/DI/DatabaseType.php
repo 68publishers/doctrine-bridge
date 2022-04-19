e8253e828ab5d4675082c80db3503542456d0046
tg666 <tomasglawaty@icloud.com>
2022-04-20 01:43:30 +0200

    Common extension for all integrations
    - removed compiler extension `Bridge\Nettrine\DI\DoctrineBridgeExtension`
    - added compiler extension `DI\DoctrineBridgeExtension`
    - added argument `DatabaseType::$commented` (BC break)
    - dropped support for YAML entity mapping driver
    - added support for Attribute entity mapping driver
    - added support for Simplified XML entity mapping driver
    - changed PHP version to `^7.4 || ^8.0`
    - updated README

--- src/DI/DatabaseType.php
+++ src/DI/DatabaseType.php
@@ -12,19 +12,23 @@ final class DatabaseType
 
 	public ?string $mappingType;
 
+	public bool $commented;
+
 	public array $context;
 
 	/**
 	 * @param string      $name
 	 * @param string      $class
-	 * @param string|NULL $mappingType
+	 * @param string|null $mappingType
+	 * @param bool        $commented
 	 * @param array       $context
 	 */
-	public function __construct(string $name, string $class, ?string $mappingType = NULL, array $context = [])
+	public function __construct(string $name, string $class, ?string $mappingType = NULL, bool $commented = FALSE, array $context = [])
 	{
 		$this->name = $name;
 		$this->class = $class;
 		$this->mappingType = $mappingType;
+		$this->commented = $commented;
 		$this->context = $context;
 	}
 }
